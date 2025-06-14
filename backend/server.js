2a836ca289c8a884d7df5014c96afa44c8ab4300
theLivingSofa <ayushagrawal10242@gmail.com>
2025-06-15 03:37:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const Task = require('./models/Task');
+const Task = require('./models/task');
 
 dotenv.config();
 
